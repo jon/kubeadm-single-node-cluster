89373e67612aec4d5a8e233f35a382f2ef3fe9ca
Jon Olson <jonolson@google.com>
2018-05-23 18:38:05 -0700

    Use cri-containerd instead of docker.

--- startup.sh
+++ startup.sh
@@ -8,14 +8,27 @@ cat <<EOF > kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 
+wget https://github.com/containerd/containerd/releases/download/v1.1.0/containerd-1.1.0.linux-amd64.tar.gz
+wget https://storage.googleapis.com/cri-containerd-release/cri-containerd-1.1.0.linux-amd64.tar.gz
+
+tar xzvf containerd-1.1.0.linux-amd64.tar.gz
+tar xzvf cri-containerd-1.1.0.linux-amd64.tar.gz
+
 sudo mv kubernetes.list /etc/apt/sources.list.d/kubernetes.list
 
 sudo apt-get update
 sudo apt-get install -y apt-transport-https
-sudo apt-get install -y docker.io
-sudo apt-get install -y kubelet kubeadm 
+sudo apt-get install -y kubelet kubeadm
 
-sudo systemctl enable docker.service
+systemctl enable containerd.service
+systemctl start containerd
+
+cat <<EOF > 0-containerd.conf
+[Service]
+Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=unix:///run/containerd/containerd.sock"
+EOF
+
+sudo mv 0-containerd.conf /etc/systemd/system/kubelet.service.d/
 
 cat <<EOF > 20-cloud-provider.conf
 Environment="KUBELET_EXTRA_ARGS=--cloud-provider=gce"
@@ -44,13 +57,14 @@ apiServerExtraArgs:
   feature-gates: AllAlpha=true
   runtime-config: api/all
 cloudProvider: gce
+criSocket: unix:///run/containerd/containerd.sock
 kubernetesVersion: ${KUBERNETES_VERSION}
 networking:
   podSubnet: 192.168.0.0/16
 noTaintMaster: true
 EOF
 
-sudo kubeadm init --config=kubeadm.conf
+sudo kubeadm init --config=kubeadm.conf --skip-preflight-checks
 
 sudo chmod 644 /etc/kubernetes/admin.conf
 
